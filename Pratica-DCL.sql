5c1cef6f2a74315e0ff922fb339bb71019ded3b1
Luiz Antonio Gomes da Silva <luiz.gsilva42@gmail.com>
2021-07-22 16:59:56 -0300

    pratica comandos DCL

---
+++ Pratica-DCL.sql
@@ -0,0 +1,96 @@
+--Parte 1 CONCEDE-GRANT
+--Cria um login e dá permissões no banco
+
+EXEC master.dbo.sp_addlogin 'UsrTeste', 'SenhaTeste';
+
+use curso;
+go
+
+--Adicionar a permissão do user no baco de dados curso
+EXEC sp_grantdbaccess 'UserTeste';
+
+--Removendo a permissão 
+--EXEC sp_revokedbaccess  'UserTest';
+
+
+--Concedendo acesso de ATUALIZAÇAO PARA UserTeste.
+GRANT UPDATE ON funcionarios TO UserTeste;
+
+--Concedendo acesso de INTERT para UserTeste.
+GRANT INSERT ON funcionarios TO UserTeste;
+
+--Conceder acesso de Leitura 
+GRANT SELECT ON funcionarios TO UserTeste;
+
+--criando procedure
+CREATE PROCEDURE testproc
+AS
+SELECT * FROM senso
+
+--executando procedure
+EXEC testproc
+
+--Concedendo acesso para EXECUTAR A PROCEDURE
+GRANT EXECUTE ON testproc TO UsrTeste
+
+--Concedendo Acesso para executar proc teste_proc para UserTreste.
+GRANT EXECUTE ON testproc TO UsrTeste
+
+
+--Verificando usuario logado
+select CURRENT_USER
+
+
+--alterando usuario logado
+SETUSER 'UsrTeste'
+
+--PARTE 2 REVOGA-REVOKE
+
+select  CURRENT_USER
+
+--REVOGANDO Acesso DE ATUALIZACAO UsrTeste.
+REVOKE UPDATE ON FUNCIONARIOS to UsrTeste; 
+
+-- REVOGANDO Acesso DE inserção UsrTeste.
+REVOKE INSERT ON FUNCIONARIOS TO UsrTeste; 
+
+-- REVOGANDO Acesso DE Leitura UsrTeste.
+REVOKE SELECT ON FUNCIONARIOS TO UsrTeste; 
+
+--REVOGA DIREITO DE EXECUCAO DA PROC TESTE_PROC PARA UsrTeste.
+REVOKE EXECUTE ON testproc TO UsrTeste ;
+
+--VERIFICANDO USUARIO LOGADO
+select CURRENT_USER
+
+--EXEC sp_grantdbaccess 'UsrTeste','UsrTeste'
+
+--EXEC sp_revokedbaccess 'UsrTeste'
+
+--ALTERANDO USUARIO LOGADO
+SETUSER 'UsrTeste'
+
+--VERIFICANDO USUARIO LOGADO
+select CURRENT_USER
+
+--EXECUTANDO PROCEDURE COM USUARIO UsrTeste
+EXEC testproc;
+
+--TESTANDO SELECT
+SELECT  * from FUNCIONARIOS
+
+--TESTANDO INSERT
+INSERT into FUNCIONARIOS values ('Maria','1000','TI')
+
+--TESTANDO UPDATE
+UPDATE FUNCIONARIOS set nome='Marilia'
+where id='8';
+
+--TESTANDO DELETE
+DELETE FROM FUNCIONARIOS
+WHERE ID='8'
+
+
+setuser;
+
+select CURRENT_USER
